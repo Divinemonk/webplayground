8a00ff3a9d49c06e52e165a26d00d2de23e2fbd9
Divinemonk <82360546+Divinemonk@users.noreply.github.com>
2023-02-14 12:55:07 +0000

    update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 console.log("script.js working!");
 
-let fn = prompt("Enter file name:");
-console.log(fn)
+// let fn = prompt("Enter file name:");
+// console.log(fn)
 
-var fs = require('fs');
-var htmlContent = 'Test File';
-fs.writeFile('./testfile.html', htmlContent, (error) => { console.log(error); });
+// var fs = require('fs');
+// var htmlContent = 'Test File';
+// fs.writeFile('./testfile.html', htmlContent, (error) => { console.log(error); });
 
-console.log("@ the end");
+// console.log("@ the end");
